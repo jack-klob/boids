7e36e498c8a9d590eb333ce4c161497f6b04cfc0
Jack Klobchar <jackklobchar@gmail.com>
2023-08-08 23:36:19 -0700

    multiple instances of a single base shape

--- res/shaders/vertex.shader
+++ res/shaders/vertex.shader
@@ -1,10 +1,11 @@
 #version 330 core
 layout(location=0) in vec2 position;
+layout(location=1) in vec2 translate;
 
 uniform mat4 u_proj;
 
 
 void main()
 {
-   gl_Position = u_proj * vec4(position, 1, 1);
+   gl_Position = u_proj * vec4(position + translate, 0, 1);
 }
--- window.cpp
+++ window.cpp
@@ -7,7 +7,7 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
-#include "shader.hpp"
+#include "shader.h"
 
 using namespace boost::numeric::ublas;
 
@@ -19,15 +19,11 @@ matrix<float, row_major> make_ortho(const float left, const float right, const f
     m(0, 0) = 2.0f / (right - left);
     m(1, 1) = 2.0f / (top - bottom);
     m(2, 2) = -2.f / (far - near);
-
-    // m(2, 0) = (right + left) / (right - left);
-    // m(2, 1) = (top + bottom) / (top - bottom);
-    // m(3, 2) = -2.f * far * near / (far - near);
+    m(3,3) = 1;
 
     m(3,0) = -(right + left) / (right - left);
     m(3,1) = -(top + bottom) / (top - bottom);
     m(3,2) = -(far + near) / (far - near);
-    m(3,3) = 1;
     return m;
 }
 
@@ -63,56 +59,67 @@ int main(void)
     // shader is program which runs on GPU
     std::cout << glGetString(GL_VERSION) << std::endl;
 
-    GLfloat positions[] = {
-        20.f, 20.f, 
-        80.f, 20.f, 
-        80.f, 50.f,
-        20.f, 50.f,
-    };
-
-    unsigned int indices[] = {
-        0,1,2,
-        2,3,0,
-    };
-
     GLuint vao;
     glGenVertexArrays(1, &vao);
     glBindVertexArray(vao);
-    
-    // vertex buffer object
-    GLuint vbo;
-    glGenBuffers(1, &vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, 2 * 4 * sizeof(GLfloat), positions, GL_STATIC_DRAW);
 
-    // this says that the layout for "positions" is located at location 0
-    // layout = 0 can then be accessed from the shader
+    // defining the shape for a boid
+    GLfloat base_tri[] = 
+    {
+        -10.f, -15.f,
+         10.f, -15.f,
+         0.f,   10.f,
+    };
+    
+    // buffer for base triangle
+    GLuint tri_buffer;
+    glGenBuffers(1, &tri_buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, tri_buffer);
+    glBufferData(GL_ARRAY_BUFFER, 3 * 2 * sizeof(GLfloat), base_tri, GL_STATIC_DRAW);
+
+    // store layout at location 0
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), 0);
 
-    // index buffer object
-    GLuint ibo;
-    glGenBuffers(1, &ibo);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
+    GLuint num_boids = 6;
+    GLfloat transform[] = {
+        400.f, 400.f,
+        100.f, 50.f,
+        20.f, 700.f,
+        700.f, 20.f,
+        15.f, 80.f,
+        342.f, 516.f,
+    };
 
-    // // project to pixel space
-    auto proj = make_ortho(0.f, 800.f, 0.f, 800.f);
+    // transform buffer
+    GLuint trans_buffer;
+    glGenBuffers(1, &trans_buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, trans_buffer);
+    glBufferData(GL_ARRAY_BUFFER, 2 * num_boids * sizeof(GLfloat), transform, GL_STATIC_DRAW);
+
+    // this says that the layout for "transform" is located at location 1
+    // layout = 1 can then be accessed from the shader
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), 0);
+    glVertexAttribDivisor(1, 1);
+
+    glBindVertexArray(vao);
 
     // create and use shaders
     GLuint shader = loadShaders("res/shaders/vertex.shader", "res/shaders/fragment.shader");
     glUseProgram(shader);
 
+    // project to pixel space
+    auto proj = make_ortho(0.f, 800.f, 0.f, 800.f);
     GLuint proj_loc = glGetUniformLocation(shader, "u_proj");
     glUniformMatrix4fv(proj_loc, 1, GL_FALSE, &proj(0,0));
-
+    
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // ibo already bound so last param can be nullptr
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+        glDrawArraysInstanced(GL_TRIANGLES, 0, 3, num_boids);
 
         glfwSwapBuffers(window); // swap front and back buffers
         glfwPollEvents(); // poll and process events
