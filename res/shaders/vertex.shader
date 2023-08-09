6b060247ea134958c495c0f8ad5fd15974bb3cc8
Jack Klobchar <jackklobchar@gmail.com>
2023-08-08 18:49:52 -0700

    projection to pixel space

--- res/shaders/vertex.shader
+++ res/shaders/vertex.shader
@@ -1,7 +1,10 @@
 #version 330 core
-layout(location=0) in vec4 position;
+layout(location=0) in vec2 position;
+
+uniform mat4 u_proj;
+
 
 void main()
 {
-    gl_Position = position;
+   gl_Position = u_proj * vec4(position, 1, 1);
 }
--- window.cpp
+++ window.cpp
@@ -2,9 +2,34 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include "boost/numeric/ublas/matrix.hpp"
+#include "boost/numeric/ublas/io.hpp"
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
 #include "shader.hpp"
+
+using namespace boost::numeric::ublas;
+
+matrix<float, row_major> make_ortho(const float left, const float right, const float bottom, const float top)
+{
+    const float near = -1.0f;
+    const float far = 1.0f;
+    matrix<float, row_major> m(4, 4, 0);
+    m(0, 0) = 2.0f / (right - left);
+    m(1, 1) = 2.0f / (top - bottom);
+    m(2, 2) = -2.f / (far - near);
+
+    // m(2, 0) = (right + left) / (right - left);
+    // m(2, 1) = (top + bottom) / (top - bottom);
+    // m(3, 2) = -2.f * far * near / (far - near);
+
+    m(3,0) = -(right + left) / (right - left);
+    m(3,1) = -(top + bottom) / (top - bottom);
+    m(3,2) = -(far + near) / (far - near);
+    m(3,3) = 1;
+    return m;
+}
 
 int main(void)
 {
@@ -14,8 +39,12 @@ int main(void)
     if (!glfwInit())
         return -1;
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(800, 800, "Flocking Simulation", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -24,6 +53,7 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
 
     if (glewInit() != GLEW_OK)
     {
@@ -31,57 +61,64 @@ int main(void)
     }
 
     // shader is program which runs on GPU
-
     std::cout << glGetString(GL_VERSION) << std::endl;
 
-    float positions[6] = {
-        -0.5f, -0.5f,
-         0.0f,  0.5f,
-         0.5f, -0.5f,
+    GLfloat positions[] = {
+        20.f, 20.f, 
+        80.f, 20.f, 
+        80.f, 50.f,
+        20.f, 50.f,
     };
 
-    unsigned int buffer;
-    glGenBuffers(1, &buffer);
-    // selecting is called binding
-    glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    // need to put data into buffer
-    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_DYNAMIC_DRAW);
+    unsigned int indices[] = {
+        0,1,2,
+        2,3,0,
+    };
+
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    
+    // vertex buffer object
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, 2 * 4 * sizeof(GLfloat), positions, GL_STATIC_DRAW);
 
     // this says that the layout for "positions" is located at location 0
     // layout = 0 can then be accessed from the shader
     glEnableVertexAttribArray(0);
-    // describe the layout of the positions buffer
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), 0);
 
-    unsigned int shader = loadShaders("res/shaders/vertex.shader", "res/shaders/fragment.shader");
+    // index buffer object
+    GLuint ibo;
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
+
+    // // project to pixel space
+    auto proj = make_ortho(0.f, 800.f, 0.f, 800.f);
+
+    // create and use shaders
+    GLuint shader = loadShaders("res/shaders/vertex.shader", "res/shaders/fragment.shader");
     glUseProgram(shader);
 
+    GLuint colour_loc = glGetUniformLocation(shader, "u_colour");
+    glUniform4f(colour_loc, 0.8f, 0.3f, 0.8f, 1.0f);
+
+    GLuint proj_loc = glGetUniformLocation(shader, "u_proj");
+    glUniformMatrix4fv(proj_loc, 1, GL_FALSE, &proj(0,0));
+
     /* Loop until the user closes the window */
-    const float increment = 0.0001f;
     while (!glfwWindowShouldClose(window))
     {
-        if(positions[2] < 1)
-        {
-            positions[2] += increment;
-        }
-        else
-        {
-            positions[2] = 0.0f;
-        }
-
-        glBindBuffer(GL_ARRAY_BUFFER, buffer);
-        // can use glBufferSubData to replace data in the vertex buffer
-        glBufferSubData(GL_ARRAY_BUFFER, 0, 6 * sizeof(float), positions);
-
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // ibo already bound so last param can be nullptr
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
-        // swap front and back buffers
-        glfwSwapBuffers(window);
-
-        // poll and process events
-        glfwPollEvents();
+        glfwSwapBuffers(window); // swap front and back buffers
+        glfwPollEvents(); // poll and process events
     }
 
     glDeleteProgram(shader);
