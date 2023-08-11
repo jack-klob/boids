955a290085f856d56637f4c41d602d7da8b265ee
Jack Klobchar <jackklobchar@gmail.com>
2023-08-10 20:29:19 -0700

    change types of matrices and add rotation matrix function

--- gl_math.cpp
+++ gl_math.cpp
@@ -1,19 +1,46 @@
 #include "gl_math.h"
+#include <cmath>
 
 using namespace boost::numeric::ublas;
 
-matrix<float, row_major> make_ortho(const float left, const float right, const float bottom, const float top)
+mat4 make_ortho(const float left, const float right, const float bottom, const float top)
 {
     const float near = -1.0f;
     const float far = 1.0f;
-    matrix<float, row_major> m(4, 4, 0);
+    mat4 m(4, 4);
     m(0, 0) = 2.0f / (right - left);
-    m(1, 1) = 2.0f / (top - bottom);
-    m(2, 2) = -2.f / (far - near);
-    m(3, 3) = 1;
+    m(0, 1) = 0.f;
+    m(0, 2) = 0.f;
+    m(0, 3) = -(right + left) / (right - left);
 
-    m(3, 0) = -(right + left) / (right - left);
-    m(3, 1) = -(top + bottom) / (top - bottom);
-    m(3, 2) = -(far + near) / (far - near);
+    m(1, 0) = 0.f;
+    m(1, 1) = 2.0f / (top - bottom);
+    m(1, 2) = 0.f;
+    m(1, 3) = -(top + bottom) / (top - bottom);
+
+    m(2, 0) = 0.f;
+    m(2, 1) = 0.f;
+    m(2, 2) = -2.f / (far - near);
+    m(2, 3) = -(far + near) / (far - near);
+
+    m(3, 0) = 0.f;
+    m(3, 0) = 0.f;
+    m(3, 0) = 0.f;
+    m(3, 3) = 1.f;
+
+    return m;
+}
+
+mat2 rotation_matrix(const float theta)
+{
+    mat2 m(2, 2);
+    float cos_theta = cos(theta);
+    float sin_theta = sin(theta);
+
+    m(0, 0) = cos_theta;
+    m(0, 1) = -sin_theta;
+    m(1, 0) = sin_theta;
+    m(1, 1) = cos_theta;
+
     return m;
 }
--- gl_math.h
+++ gl_math.h
@@ -3,10 +3,16 @@
 
 #include "boost/numeric/ublas/matrix.hpp"
 #include "boost/numeric/ublas/io.hpp"
+#include <boost/numeric/ublas/vector.hpp>
+#include <boost/numeric/ublas/storage.hpp>
 
-using namespace boost::numeric::ublas;
+using vec2 = boost::numeric::ublas::bounded_vector<float, 2>;
+// matrix is stored in column major order as openGL expects
+using mat2 = boost::numeric::ublas::bounded_matrix<float, 2, 2, boost::numeric::ublas::column_major>;
+using mat4 = boost::numeric::ublas::bounded_matrix<float, 4, 4, boost::numeric::ublas::column_major>;
 
-boost::numeric::ublas::matrix<float, boost::numeric::ublas::row_major> make_ortho(
-    const float left, const float right, const float bottom, const float top);
+mat4 make_ortho(const float left, const float right, const float bottom, const float top);
+
+mat2 rotation_matrix(const float theta);
 
 #endif
