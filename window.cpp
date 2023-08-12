242e88ee9354d8eb95e34c651e0afadd1701777c
Jack Klobchar <jackklobchar@gmail.com>
2023-08-12 04:41:28 -0700

    add use of program options (not yet implemented in sim)

--- window.cpp
+++ window.cpp
@@ -6,6 +6,7 @@
 #include "gl_math.h"
 #include <chrono>
 #include <iostream>
+#include <boost/program_options.hpp>
 
 class FrameLimiter
 {
@@ -38,8 +39,77 @@ private:
     std::chrono::steady_clock::time_point prev_update_ = std::chrono::steady_clock::now();
 };
 
-int main(void)
+namespace po = boost::program_options;
+
+struct parameters
 {
+    float cohesion_factor;
+    float alignment_factor;
+    float separation_factor;
+    std::size_t n;
+    unsigned int seed;
+    float sight_dist;
+    float sight_angle;
+    int height;
+    int width;
+};
+
+parameters handle_arguments(int argc, char* argv[])
+{
+    auto range = [](float min, float max, char const *const opt_name)
+    {
+        return [opt_name, min, max](float v)
+        {
+            if(v < min || v > max)
+            {
+                throw po::validation_error(
+                    po::validation_error::invalid_option_value, opt_name, std::to_string(v));
+            }
+        };
+    };
+
+    parameters params;
+    
+    
+    po::options_description desc("Allowed options");
+    try
+    {
+        desc.add_options()
+            ("help,h", "help screen")
+            ("cohesion", po::value<float>(&params.cohesion_factor)->default_value(0.5f)->notifier(range(0.f, 1.f, "cohesion")), "set cohesion factor | range [0.0, 1.0]")
+            ("alignment", po::value<float>(&params.alignment_factor)->default_value(0.5f)->notifier(range(0.f, 1.f, "alignment")), "set alignment factor | range [0.0, 1.0]" )
+            ("separation", po::value<float>(&params.separation_factor)->default_value(0.5f)->notifier(range(0.f, 1.f, "separation")), "set separation factor | range [0.0, 1.0]")
+            ("n", po::value<std::size_t>(&params.n)->default_value(50), "number of boids")
+            ("seed", po::value<unsigned int>(), "seed for random number generator used")
+            ("sight-distance", po::value<float>(&params.sight_dist)->default_value(50)->notifier(range(0.f, INFINITY, "sight-distance")), "boid sight distance (pixels) | range [0.0, inf)")
+            ("sight-angle", po::value<float>(&params.sight_angle)->default_value(90.f)->notifier(range(0.f, 360.f, "sight-angle")), "boid field of view (degrees) | range [0.0, 360.0]");
+
+        po::variables_map vm;
+        po::store(po::parse_command_line(argc, argv, desc), vm);
+        po::notify(vm);
+
+        if (vm.count("help"))
+        {
+            std::cout << desc << '\n';
+            exit(0);
+        }if(vm.count("seed"))
+        {
+            params.seed = vm["seed"].as<unsigned int>();
+        }
+    }
+    catch(std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+        exit(1);
+    }
+    return params;
+}
+
+int main(int argc, char* argv[])
+{
+
+    parameters params = handle_arguments(argc, argv);
+
     GLFWwindow *window;
 
     /* Initialize the library */
@@ -50,7 +120,8 @@ int main(void)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    // Create a windowed mode window and its OpenGL context
+    // create window
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
     window = glfwCreateWindow(800, 800, "Flocking Simulation", NULL, NULL);
     if (!window)
     {
